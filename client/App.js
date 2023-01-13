986903b797636eee63c7e6328e5dbc5741703490
mahavirjain11 <74929856+mahavirjain11@users.noreply.github.com>
2023-01-13 22:34:12 +0530

    Create App.js

---
+++ client/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CustomerForm from "./components/CustomerForm";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CustomerForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
